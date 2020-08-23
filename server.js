448f6a0536b6fd2e6aac57343142bfc96111e1b8
letsgitthis <jbentley1011@gmail.com>
2020-08-22 20:03:56 -0500

    building server

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// localhost port
+// localhost port is declared
 const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
+// create routes using express
+const app = express();
+
+// mongoose handles the schema 
+
+// mongo is the db (database)
+//===========================
+
+// listen for port availability and then host 
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
